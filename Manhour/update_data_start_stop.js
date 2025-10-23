48a97c24d5d7b75618efd048fddfc5764becde0f
oticstps <oticsjetson@gmail.com>
2025-10-23 18:11:22 +0700

    Update update_data_start_stop.js

--- Manhour/update_data_start_stop.js
+++ Manhour/update_data_start_stop.js
@@ -1,14 +1,11 @@
 const pool = require("./config/db");
 
-async function syncOneTable(index) {
-  const sourceTable = `common_rail_${index}`;
+async function syncOneTable(sourceTable) {
   const targetTable = `${sourceTable}_transit_manhour`;
-
   const connection = await pool.getConnection();
+
   try {
     console.log(`\n🚀 Mulai sinkronisasi data dari ${sourceTable}...`);
-
-
     const [rows] = await connection.query(`
       SELECT *
       FROM ${sourceTable}
@@ -22,17 +19,14 @@ async function syncOneTable(index) {
 
     console.log(`📦 ${sourceTable}: ditemukan ${rows.length} data.`);
 
-
     await connection
       .query(`
         ALTER TABLE ${targetTable}
         ADD UNIQUE INDEX IF NOT EXISTS uniq_transit (line_id, status, created_at)
       `)
       .catch(() => {});
-
     let inserted = 0;
 
-    // --- Loop insert dengan INSERT IGNORE
     for (const row of rows) {
       await connection.query(
         `
@@ -71,14 +65,25 @@ async function syncOneTable(index) {
   }
 }
 
+
 async function syncAllTables() {
   console.log("=== 🚀 Mulai sinkronisasi semua common_rail_X ===");
-  for (let i = 1; i <= 12; i++) {
-    await syncOneTable(i);
-  }
+
+  await syncOneTable("common_rail_1");
+  await syncOneTable("common_rail_2");
+  await syncOneTable("common_rail_3");
+  await syncOneTable("common_rail_4");
+  await syncOneTable("common_rail_5");
+  await syncOneTable("common_rail_6");
+  await syncOneTable("common_rail_7");
+  await syncOneTable("common_rail_8");
+  await syncOneTable("common_rail_9");
+  await syncOneTable("common_rail_10");
+  await syncOneTable("common_rail_11");
+  await syncOneTable("common_rail_12");
+
   console.log("\n✅ Semua tabel sudah disinkronisasi dengan aman.");
 }
-
 
 
 
